94ef31195bd0d2804de664c91429b915cecc4c6b
Ali Abbaszade <abbasszade.a@gmail.com>
2025-05-20 00:13:02 +0330

    Added shopping list
    - Added shopping list model
    - Added Shopping list serializer
    - Updated views

--- shopping_list/admin.py
+++ shopping_list/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 
 @admin.register(ShoppingItem)
 class ShoppingItemAdmin(admin.ModelAdmin):
-    list_display = ["name", "purchased"]
+    list_display = ["name", "purchased", "shopping_list"]
+
+
+class ShoppingItemInline(admin.StackedInline):
+    model = ShoppingItem
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    inlines = [ShoppingItemInline]
--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,11 +1,28 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 
-from shopping_list.api import viewsets
 
-router = DefaultRouter()
-router.register("shopping-items", viewsets.ShoppingItemViewSet)
+from shopping_list.api import views
+
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    path(
+        "api/shopping-lists/",
+        views.ListAddShoppingList.as_view(),
+        name="all-shopping-lists",
+    ),
+    path(
+        "api/shopping-lists/<uuid:pk>/",
+        views.ShoppingListDetail.as_view(),
+        name="shopping-list-detail",
+    ),
+    path(
+        "api/shopping-lists/<uuid:pk>/shopping-items/",
+        views.AddShoppingItem.as_view(),
+        name="add-shopping-item",
+    ),
+    path(
+        "api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>/",
+        views.ShoppingItemDetail.as_view(),
+        name="shopping-item-detail",
+    ),
 ]
